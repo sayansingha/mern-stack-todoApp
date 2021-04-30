9d88c78c8d15a8041e3d4cebffafab7133110402
sayansingha <sayansinghargj@gmail.com>
2021-04-30 22:13:49 +0530

    added build

---
+++ frontend/build/precache-manifest.0c1fca6c60102c8243441df318a0a16e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def4e697dd5f5b04f5df72c17a07ce4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9fd853bd026a92850f2",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7c4f649c14785f3e8fcb",
+    "url": "/static/css/main.c90f571a.chunk.css"
+  },
+  {
+    "revision": "d9fd853bd026a92850f2",
+    "url": "/static/js/2.0c79d774.chunk.js"
+  },
+  {
+    "revision": "7c4f649c14785f3e8fcb",
+    "url": "/static/js/main.1b6f8521.chunk.js"
+  },
+  {
+    "revision": "32f9b85d480e8674cb7f",
+    "url": "/static/js/runtime-main.d821acc1.js"
+  },
+  {
+    "revision": "8cb052610a64040fe4a718a382f395eb",
+    "url": "/static/media/logo1.8cb05261.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express()
 const bodyParser=require('body-parser')
 const cors = require('cors')
+const path = require('path');
 const mongoose = require('mongoose')
 const todoRoutes = express.Router()
 const PORT= 4000
 
-let Todo = require('./todo.model.js')
-
-app.use(cors())
-app.use(bodyParser.json())
-mongoose.connect('mongodb://127.0.0.1:27017/todos',{ useNewUrlParser: true})
+let Todo = require('./backend/todo.model.js')
+const mongoUrl = 'mongodb+srv://sayan:fBJZHG8Fq1SncQnG@cluster0.ovm15.mongodb.net/todo?retryWrites=true&w=majority';
+app.use(express.static(path.resolve("./frontend/build")))
+// app.use(cors())
+// app.use(bodyParser.json())
+mongoose.connect(mongoUrl,{ useNewUrlParser: true, useUnifiedTopology: true })
 const connection = mongoose.connection
 
 connection.once('open',()=>{
